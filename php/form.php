a36dff8e864e2fe40ad285915bd974088a36218e
Nivlagx <gchi908@aucklanduni.ac.nz>
2016-03-08 01:02:15 +1300

    Updated php

--- php/form.php
+++ php/form.php
@@ -3,5 +3,13 @@ $name = $_POST['userName'];
 $email = $_POST['userEmail'];
 $message = $_POST['userMessage'];
 
-echo "Hello world!";
+$to = 'galvin9@hotmail.com'; // put your email
+$email_subject = "Contact form submitted by:  $name";
+$email_body = "You have received a new message. \n\n".
+				  " Here are the details:\n \nName: $name \n ".
+				  "Email: $email\n Message \n $message";
+$headers = "From: test@test.com\n";
+$headers .= "Reply-To: $email";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
 ?>
