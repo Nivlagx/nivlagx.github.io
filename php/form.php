927564be8ccf67de6456e2dd962525a8a62c5bb0
Nivlagx <galvin9@hotmail.com>
2016-03-08 22:34:27 +1300

    Updated php email

--- php/form.php
+++ php/form.php
@@ -4,12 +4,11 @@ $email = $_POST['userEmail'];
 $message = $_POST['userMessage'];
 
 $to = 'galvin9@hotmail.com';
-$email_subject = "Contact form submitted by:  $name";
+$email_subject = "Contact form submitted by: $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email\n Message \n $message";
 $headers = "From: test@test.com\n";
-$headers .= "Reply-To: $email";	
-//mail($to,$email_subject,$email_body,$headers);
-mail('galvin9@hotmail.com','subject test','body test','From test');				
+$headers = "Reply-To: $email";	
+mail($to,$email_subject,$email_body,$headers);		
 ?>
