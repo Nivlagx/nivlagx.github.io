814ee4ec6ca7a2eadf3b659e439078301bee23f7
Nivlagx <gchi908@aucklanduni.ac.nz>
2016-03-14 17:22:09 +1300

    php mail function test

--- php/form.php
+++ php/form.php
@@ -8,8 +8,14 @@ $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email\n Message \n $message";
-$headers = "From: test@test.com\n";
-$headers .= "Reply-To: $email";	
-//mail($to,$email_subject,$email_body,$headers);
-mail('galvin9@hotmail.com','subject test','body test','From test');				
+$headers =  'MIME-Version: 1.0' . "\r\n"; 
+$headers .= 'From: header test <galvin9@hotmail.com>' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+$retval = mail('galvin9@hotmail.com','php test','test',$headers);
+//mail($to,$email_subject,$email_body,$headers);		
+if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }	
 ?>
